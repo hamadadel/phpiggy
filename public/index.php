bdd17e48447af4b28690d8b954b2c51ff0f833fe
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-05 17:40:17 +0300

    created a Router class and made it a component or tool from the App class

--- public/index.php
+++ public/index.php
@@ -2,5 +2,8 @@
 declare(strict_types = 1);
 // realpath(__DIR__.'/../src/App');
 
+require_once __DIR__.'/../src/App/functions.php';
 $app = require_once __DIR__ . '/../src/App/bootstrap.php';
 $app->run();
+
+dd($app);
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -9,6 +9,12 @@ namespace Framework;
 
 class App
 {
+    private Router $router;
+    
+    public function __construct()
+    {
+        $this->router = new Router;
+    }
 
     public function run()
     {
