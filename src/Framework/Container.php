1ca717581ee714a8f1a6595b0e81103f4aa6855f
Hamad Adel <hamadwebdeveloper@gmail.com>
2023-09-12 15:06:15 +0300

    create extractDependencies()@Container, whih create an array of dependencies required to the controller

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -8,6 +8,7 @@ declare(strict_types = 1);
 
 namespace Framework;
 
+use Framework\Exceptions\ContainerException;
 use ReflectionClass;
 
 class Container
@@ -22,9 +23,39 @@ class Container
     public function resolve( string $controller)
     {
         $reflectionClass = new ReflectionClass($controller);
-        if (! $reflectionClass->isInstantiable()) {
-            dd($reflectionClass);
+        if (! $reflectionClass->isInstantiable())
+            throw new ContainerException("class {$controller} is not Instantiable");
+
+        $constructor = $reflectionClass->getConstructor();
+        if (! $constructor)
+            return new $controller;
+
+        $parameters = $constructor->getParameters();
+        if (!$parameters)
+            return new $controller;
+
+        $dependencies = $this->extractDependencies($parameters, $controller);
+
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    private function extractDependencies(array $parameters, string $controller): array
+    {
+        $dependencies = [];
+        foreach ($parameters as $parameter) {
+            $name = $parameter->getName();
+            $type = $parameter->getType();
+            if (! $type)
+                throw new ContainerException("Failed to resolve class {$controller}, parameter {$name} is missing a type hint");
+
+            if (! $type instanceof \ReflectionNamedType || $type->isBuiltin())
+                throw new ContainerException("Failed to resolve class {$controller}, invalid parameter name.");
+
+            if (!array_key_exists($type->getName(), $this->definitions))
+                throw new ContainerException("Class ${$controller} does not exist in the Container.");
+
+            $dependencies[]= $this->definitions[$type->getName()]();
         }
-        return $controller;
+        return $dependencies;
     }
 }
